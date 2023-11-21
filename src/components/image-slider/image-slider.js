d486c3375aa25d1c93497b1cb356bc8dcc42c157
danielkurtsawyer <19danielsawyer@gmail.com>
2023-11-21 14:11:27 -0500

    Implement Image Slider Component

--- src/components/image-slider/image-slider.js
+++ src/components/image-slider/image-slider.js
@@ -1,5 +1,147 @@
 import './image-slider.css';
+import Left from './chevron-left.svg';
+import Right from './chevron-right.svg';
 
-export default () => {
-  console.log('Hello world');
+const updateVisibility = function updateArrowVisbility(
+  newPictureIndex,
+  numPictures,
+  leftArrow,
+  rightArrow
+) {
+  if (
+    newPictureIndex === 0 &&
+    !leftArrow.classList.contains('image-slider-arrow-invisible')
+  ) {
+    leftArrow.classList.add('image-slider-arrow-invisible');
+  } else if (
+    newPictureIndex > 0 &&
+    leftArrow.classList.contains('image-slider-arrow-invisible')
+  ) {
+    leftArrow.classList.remove('image-slider-arrow-invisible');
+  }
+
+  if (
+    newPictureIndex === numPictures - 1 &&
+    !rightArrow.classList.contains('image-slider-arrow-invisible')
+  ) {
+    rightArrow.classList.add('image-slider-arrow-invisible');
+  } else if (
+    newPictureIndex < numPictures &&
+    rightArrow.classList.contains('image-slider-arrow-invisible')
+  ) {
+    rightArrow.classList.remove('image-slider-arrow-invisible');
+  }
 };
+
+const changeFrame = function changeFrameImage(
+  allImagesDiv,
+  newPictureIndex,
+  numPictures
+) {
+  if (newPictureIndex >= 0 && newPictureIndex < numPictures) {
+    // change transform property
+    allImagesDiv.setAttribute(
+      'style',
+      `transform: translate(-${newPictureIndex * 100}%)`
+    );
+
+    const leftArrow = document.querySelector('.image-slider-left-arrow');
+    const rightArrow = document.querySelector('.image-slider-right-arrow');
+
+    // update arrows image-slider-index attribute
+    leftArrow.setAttribute('image-slider-index', newPictureIndex - 1);
+    rightArrow.setAttribute('image-slider-index', newPictureIndex + 1);
+
+    // also update visibility class if no other image in range
+    setTimeout(
+      () =>
+        updateVisibility(newPictureIndex, numPictures, leftArrow, rightArrow),
+      250
+    );
+  }
+
+  console.log('clicked!');
+};
+
+export default function initializeImageSlider(...pictures) {
+  // create a container to hold the image slider elements
+  const imageSlider = document.createElement('div');
+  imageSlider.classList.add('image-slider-container');
+
+  // create the left arrow
+  const leftArrow = new Image();
+  leftArrow.src = Left;
+  leftArrow.classList.add('image-slider-left-arrow');
+  leftArrow.setAttribute('image-slider-index', -1);
+  // initially, there is no image to the left so add the invisible class
+  leftArrow.classList.add('image-slider-arrow-invisible');
+
+  imageSlider.appendChild(leftArrow);
+
+  // create the frame to hold the images
+  const frame = document.createElement('div');
+  frame.classList.add('image-slider-frame');
+
+  imageSlider.appendChild(frame);
+
+  // create a div to hold all of the images in a long grid
+  const allImagesDiv = document.createElement('div');
+  allImagesDiv.classList.add('image-slider-images');
+
+  // add every picture to the div
+  pictures.forEach((picture) => {
+    const img = new Image();
+    img.src = picture;
+    img.classList.add('image-slider-img');
+    allImagesDiv.appendChild(img);
+  });
+
+  // add to the frame
+  // the CSS formats the image div width to cover exactly one image within the frame
+  frame.appendChild(allImagesDiv);
+
+  // create the right arrow
+  const rightArrow = new Image();
+  rightArrow.src = Right;
+  rightArrow.classList.add('image-slider-right-arrow');
+  rightArrow.setAttribute('image-slider-index', 1);
+
+  // add event listeners for user interactivity
+
+  leftArrow.addEventListener('click', (e) =>
+    changeFrame(
+      allImagesDiv,
+      +e.target.getAttribute('image-slider-index'),
+      pictures.length
+    )
+  );
+
+  rightArrow.addEventListener('click', (e) =>
+    changeFrame(
+      allImagesDiv,
+      +e.target.getAttribute('image-slider-index'),
+      pictures.length
+    )
+  );
+
+  imageSlider.appendChild(rightArrow);
+
+  // next, create the navigation dots at the bottom
+  const navDotContainer = document.createElement('div');
+  navDotContainer.classList.add('image-slider-nav-dot-container');
+
+  pictures.forEach((picture, index) => {
+    console.log(index);
+    const navDot = document.createElement('div');
+    navDot.classList.add('image-slider-nav-dot');
+    navDot.setAttribute('image-slider-index', index);
+    navDot.addEventListener('click', () =>
+      changeFrame(allImagesDiv, index, pictures.length)
+    );
+    navDotContainer.appendChild(navDot);
+  });
+
+  imageSlider.appendChild(navDotContainer);
+
+  return imageSlider;
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import createMobileMenu from './components/mobile-menu/mobile-menu.js';
 import createImageSlider from './components/image-slider/image-slider.js';
 import './index.css';
 
+import img1 from './img/bear.jpeg';
+import img2 from './img/elephant.jpeg';
+import img3 from './img/wolf.jpeg';
+
 const body = document.querySelector('body');
 const dropDownDiv = document.createElement('div');
 
@@ -14,12 +18,7 @@ dropDownDiv.appendChild(
   createDropDownMenu(
     'New Menu',
     ['Option1', 'Option2', 'Option3', 'areallylongoptionnamegoeshere'],
-    [
-      'https://www.google.com',
-      'https://www.google.com',
-      'https://www.google.com',
-      'https://www.google.com',
-    ]
+    ['#', '#', '#', '#']
   )
 );
 
@@ -27,12 +26,7 @@ dropDownDiv.appendChild(
   createDropDownMenu(
     'Another Menu',
     ['Option1', 'Option2', 'Option3', 'areallylongoptionnamegoeshere'],
-    [
-      'https://www.google.com',
-      'https://www.google.com',
-      'https://www.google.com',
-      'https://www.google.com',
-    ]
+    ['#', '#', '#', '#']
   )
 );
 
@@ -45,13 +39,12 @@ mobileMenuDiv.appendChild(
   createMobileMenu(
     'Mobile Menu',
     ['Option1', 'Option2', 'Option3', 'areallylongoptionnamegoeshere'],
-    [
-      'https://www.google.com',
-      'https://www.google.com',
-      'https://www.google.com',
-      'https://www.google.com',
-    ]
+    ['#', '#', '#', '#']
   )
 );
 
-createImageSlider();
+const imageSliderDiv = document.createElement('div');
+imageSliderDiv.classList.add('image-slider');
+
+body.appendChild(imageSliderDiv);
+imageSliderDiv.appendChild(createImageSlider(img1, img2, img3));
